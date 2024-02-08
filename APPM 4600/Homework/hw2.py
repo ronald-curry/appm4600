345fced8046fe09dff39a26848f28766569c71b1
ronald-curry <rocu1744@colorado.edu>
2024-02-07 21:17:08 -0700

    Update hw2.py

--- APPM 4600/Homework/hw2.py
+++ APPM 4600/Homework/hw2.py
@@ -7,6 +7,7 @@ Created on Wed Feb  7 19:18:53 2024
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 '''
 t=np.zeros(31);
 #print(t[0])
@@ -16,7 +17,7 @@ y=np.cos(t)
 print("The sum, S, is:")
 print(np.sum(y))
 '''
-
+'''
 theta=np.linspace(0,2*np.pi)
 R=1.2
 delta_r=0.1
@@ -35,3 +36,10 @@ for i in range (10):
     y=R*(1+delta_r*np.sin(f*theta+p))*np.sin(theta)
     plt.plot(x,y)
 plt.show()
+'''
+
+x=9.999999995000000 * 10**(-10)
+y=math.e**x
+y=y-1
+print("y calc using e^")
+print(y)
