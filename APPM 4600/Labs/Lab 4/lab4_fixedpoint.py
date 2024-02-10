12729bc00d18188c29caf133b1eb35ba7343d6e5
ronald-curry <rocu1744@colorado.edu>
2024-02-10 11:31:33 -0700

    changed fixed point to output each guess in array

---
+++ APPM 4600/Labs/Lab 4/lab4_fixedpoint.py
@@ -0,0 +1,40 @@
+# import libraries
+import numpy as np
+    
+# define routines
+def fixedpt(f,x0,tol,Nmax):
+
+    ''' x0 = initial guess''' 
+    ''' Nmax = max number of iterations'''
+    ''' tol = stopping tolerance'''
+    x = np.zeros((Nmax+1,1))
+    count = 0
+    x[0]=x0
+    while (count <Nmax):
+        x1 = f(x0)
+        count = count +1
+        x[count]=x1
+        if (abs(x1-x0) <tol):
+            xstar = x1
+            ier = 0
+            return [xstar,ier,x]
+        x0 = x1
+
+    xstar = x1
+    ier = 1
+    return [xstar, ier,x]
+    
+
+# use routines 
+f1 = lambda x: x-(x**5-7)/(5*x**4)
+
+Nmax = 50
+tol = 1e-8
+
+''' test f1 '''
+x0 = 1
+[xstar,ier,x] = fixedpt(f1,x0,tol,Nmax)
+print('the approximate fixed point is:',xstar)
+print('f1(xstar):',f1(xstar))
+print('Error message reads:',ier)
+    
