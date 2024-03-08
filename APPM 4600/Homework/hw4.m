1159d57f741047be73ca385d0833fe78b9fb2522
ronald-curry <rocu1744@colorado.edu>
2024-03-08 12:11:08 -0700

    verctorized code instead of compentenwise math

--- APPM 4600/Homework/hw4.m
+++ APPM 4600/Homework/hw4.m
@@ -1,32 +1,38 @@
 clc; close all; clear;
 
 
-x0=1;
-y0=-1;
-nMax=400;
-tol=10^(-10);
+x0=[1.3;0;0];
+nMax=30;
+tol=10^(-6);
+
 tic;
-guess1=newton(x0,y0,nMax,tol);
+guess1=newton(x0,nMax,tol);
 toc
-tic;
-guess2=Lazy(x0,y0,nMax,tol);
 
-toc;
 tic;
-guess3=broyden(x0,y0,nMax,tol);
+guess2=steepest(x0,nMax,tol);
+toc; 
+
+tic;
+guess3=broyden(x0,nMax,tol);
 toc;
 
-function [o]=f(x,y)
-    o=x.^2+y.^2-4;
+function [o]=f(x)
+    o=x(1,:)+cos(x(1,:).*x(2,:).*x(3,:))-1;
 end
 
-function [o]=g(x,y)
-    o=exp(x)+y-1;
+function [o]=g(x)
+    o=(1-x(1,:)).^(1/4)+x(2,:)+0.05*x(3,:).^2-0.15*x(3,:)-1;
 end
 
+function [o]=h(x)
+    o=-x(1,:).^2-0.1*x(2,:).^2+0.01*x(2,:)+x(3,:)-1;
+end
 
-function [j]=J(x,y)
-    j=[2*x,2*y;exp(x),1];
+function [j]=J(x)
+    j=[1-sin(x(1,:).*x(2,:).*x(3,:)).*x(2,:).*x(3,:),-sin(x(1,:).*x(2,:).*x(3,:)).*x(1,:).*x(3,:),-sin(x(1,:).*x(2,:).*x(3,:)).*x(2,:).*x(1,:);
+    (1/4)*(1-x(1,:)).^(-3/4),1,0.1*x(3,:)-0.15;
+    -2*x(1,:),-0.2*x(2,:)+0.01,1];
 end
 
 function guess=fixedpoint(x0,y0,nMax,tol)
@@ -53,29 +59,21 @@ function guess=fixedpoint(x0,y0,nMax,tol)
     title("Fixed Point")
 end
 
-function [guess]=newton(x0,y0,nMax,tol)
-    x=NaN(2,nMax+1);
-    error=NaN(3,nMax+1);
-    x(:,1)=[x0;y0];
-    for n=1:nMax
-        j = J(x(1,n),x(2,n));
-        F = [f(x(1,n),x(2,n));g(x(1,n),x(2,n))];
-        x(:,n+1)=x(:,n)-j\F;
-        if sqrt((f(x(1,n+1),x(2,n+1)))^2+(g(x(1,n+1),x(2,n+1)))^2)<tol %checks if norm(f(n),g(n))<tol
-            guess=[x(1,n+1);x(2,n+1)]; %final guess [x;y]
-            break
-        end
-        if n==nMax
-            error('did not converge to tol in Nmax')
-        end
-    end
+function [guess]=newton(x0,nMax,tol)
+    fun=@(x) ([f(x);g(x);h(x)]);
+    Jfun=@(x) J(x);
+    verb=0;
+    [guess,x] = newton_method_nd(fun,Jfun,x0,tol,nMax,verb);
+    x=cat(2,x0,x);
     figure()
-    error(1,:)=abs(f(x(1,:),x(2,:)));
-    error(2,:)=abs(g(x(1,:),x(2,:)));
-    error(3,:)=sqrt(error(1,:).^2+error(2,:).^2);
-    plot(1:nMax+1,log10(error(3,:)))
+    error=NaN(4,length(x(1,:)));
+    error(1,:)=abs(g(x));
+    error(2,:)=abs(g(x));
+    error(3,:)=abs(h(x));
+    error(4,:)=sqrt(error(1,:).^2+error(2,:).^2+error(3,:).^2);
+    plot(1:length(x(1,:)),log10(error(4,:)))
     xlabel("n")
-    ylabel("log_{10}(error)")
+    ylabel("log_{10}(error)")    
     title("Newton")  
 end
 
@@ -105,20 +103,20 @@ function [guess]=Lazy(x0,y0,nMax,tol)
     title("Lazy Newton")  
 end
 
-function [guess]=broyden(x0,y0,nMax,tol)
-    B0=J(x0,y0);
-    fun=@(x) ([f(x(1),x(2));g(x(1),x(2))]);
-    x=[x0;y0];
+function [guess]=broyden(x0,nMax,tol)
+    B0=J(x0);
+    fun=@(x) ([f(x);g(x);h(x)]);
     Bmat='fwd';
     verb=0;
-    [guess,x] = broyden_method_nd(fun,B0,x,tol,nMax,Bmat,verb);
-    x=cat(2,[x0;y0],x);
+    [guess,x] = broyden_method_nd(fun,B0,x0,tol,nMax,Bmat,verb);
+    x=cat(2,x0,x);
     figure()
-    error=NaN(3,length(x(1,:)));
-    error(1,:)=abs(f(x(1,:),x(2,:)));
-    error(2,:)=abs(g(x(1,:),x(2,:)));
-    error(3,:)=sqrt(error(1,:).^2+error(2,:).^2);
-    plot(1:length(x(1,:)),log10(error(3,:)))
+    error=NaN(4,length(x(1,:)));
+    error(1,:)=abs(g(x));
+    error(2,:)=abs(g(x));
+    error(3,:)=abs(h(x));
+    error(4,:)=sqrt(error(1,:).^2+error(2,:).^2+error(3,:).^2);
+    plot(1:length(x(1,:)),log10(error(4,:)))
     xlabel("n")
     ylabel("log_{10}(error)")
     title("Broyden")
@@ -223,7 +221,6 @@ end
 
 end
 
-
 function y = Inapp(I0,Un,Vn,x)
 %{
 Function that applies I0*x + Un*Vn.'*x depending on a few cases for the
@@ -242,6 +239,146 @@ else
    else
        y = I0(x) + Un*(Vn.'*x);
    end
+end
+
+end
+
+function [guess]=steepest(x0,nMax,tol)
+    %fun=
+    Gfun=@(x) ([f(x);g(x);h(x)]);
+    type="wolfe";
+    verb=0;
+    [guess,x] = steepest_descent(fun,Gfun,x0,tol,nMax,type,verb);
+    x=cat(2,x0,x);
+    figure()
+    error=NaN(4,length(x(1,:)));
+    error(1,:)=abs(g(x));
+    error(2,:)=abs(g(x));
+    error(3,:)=abs(h(x));
+    error(4,:)=sqrt(error(1,:).^2+error(2,:).^2+error(3,:).^2);
+    plot(1:length(x(1,:)),log10(error(4,:)))
+    xlabel("n")
+    ylabel("log_{10}(error)")
+    title("Steepest Descent")
+end
+
+function [r,rn] = steepest_descent(fun,Gfun,x0,tol,nmax,type,verb)
+%{
+This Matlab function finds, if possible, a local minimum for the  
+differentiable function fun with gradient Gfun using the basic 
+Gradient descent step starting from initial guess x0. 
+Step size is restricted with a simple back-tracking line search strategy. 
+
+It is generally assumed that the function is locally convex at x0 
+(the Hessian Hfun(x0) is symmetric, positive definite) 
+ 
+The method runs until |fun(xn)|<tol or nmax iterations are reached. 
+type is a string indicating line search type and verb is a boolean (0 or 1)
+controlling whether info is printed or not on the command line. 
+
+Inputs: 
+fun - (function or function handle) function fun(x), x is
+assumed to be an n x 1 array
+Gfun - (function or function handle) Gradient vector of size n x 1
+x0   - (n x 1 double) initial guess for minimum
+tol - (double) target accuracy / tolerance for the algorithm
+nmax - (int) max number of iterations
+type - (string) line search type. Options are 'wolfe', 'swolfe' and
+'armijo' (default). 
+verb - (bool) print and pause (1) or don't print or pause (0)
+
+Outputs:
+r   - (n x 1 double) final estimate for the minimum
+rn  - (n x niter double array) vector of iterates xn (used mainly for convergence /
+testing purposes). 
+
+Instructor: Eduardo Corona
+%}
+
+params.c1 = 10^-3; params.c2 = 0.9; params.maxback=10; xn=x0; 
+n=0; rn(:,1)=x0; 
+alpha=1; 
+fn = fun(xn); 
+pn = -Gfun(xn); 
+
+if verb
+fprintf('\n|--n--|-alpha-|----|xn|----|---|f(xn)|---|---|Gf(xn)|---|')
+end
+
+while n<=nmax && norm(pn)>tol
+    if verb
+    fprintf('\n|--%d--|%1.5f|%1.7f|%1.7f|%1.7f|',n,alpha,norm(xn),abs(fn),norm(pn));  
+    %pause(0.01); 
+    end
+    
+    [alpha,xn] = line_search(fun,Gfun,xn,pn,type,params);
+    fn = fun(xn); 
+    pn = -Gfun(xn);
+    n=n+1; 
+    rn(:,n+1)=xn; 
+end
+
+r=xn; 
+
+end
+
+function [r,rn] = newton_method_nd(fun,Jfun,x0,tol,nmax,verb)
+%{
+This Matlab function finds, if possible, a root for the multivariable, 
+differentiable function fun. Newton method runs until
+|fun(xn)|<tol or nmax iterations are reached. verb is a boolean (0 or 1)
+controlling whether info is printed or not on the command line. 
+
+Inputs: 
+fun - (function or function handle) multivariable function fun(x), x is
+assumed to be an n x 1 array
+Jfun - (function or function handle) Jacobian matrix of size n x n  
+x0   - (n x 1 double) initial guess for root
+tol - (double) target accuracy / tolerance for the algorithm
+nmax - (int) max number of iterations
+verb - (bool) print and pause (1) or don't print or pause (0)
+
+Outputs:
+r   - (n x 1 double) final estimate for the root
+rn  - (n x niter double array) vector of iterates xn (used mainly for convergence /
+testing purposes). 
+
+Instructor: Eduardo Corona
+%}
+
+if nargin<6
+    verb=0; 
+end
+
+% Initialize iteration and function value
+xn=x0; rn(:,1)=x0; Fn = fun(xn); npn=1; 
+
+n=0; 
+if verb
+fprintf('\n|--n--|----xn----|---|f(xn)|---|')
+end
+while npn>tol && n<=nmax
+    Jn=Jfun(xn);  
+    
+    if verb
+    fprintf('\n|--%d--|%1.7f|%1.7f|',n,norm(xn),norm(Fn));  
+    %pause; 
+    end
+    
+    % Newton step x_{n+1} = x_n - Jf(x_n)^{-1} * F(x_n) 
+    pn = -Jn\Fn;
+    xn = xn + pn;
+    npn = norm(pn); 
+    
+    n=n+1; 
+    rn(:,n)=xn;
+    Fn = fun(xn); 
+end
+    
+r=xn;
+    
+if npn>tol
+   fprintf('\n Newton method failed to converge, n=%d, res=%e\n',nmax,norm(Fn)); 
 end
 
 end
