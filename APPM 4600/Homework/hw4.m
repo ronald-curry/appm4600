7b1bd722301b87c6d008998c0a4efb24eda44335
ronald-curry <rocu1744@colorado.edu>
2024-03-08 16:53:39 -0700

    finished hw5

--- APPM 4600/Homework/hw4.m
+++ APPM 4600/Homework/hw4.m
@@ -4,17 +4,22 @@ clc; close all; clear;
 x0=[1.3;0;0];
 nMax=30;
 tol=10^(-6);
+tol1=5*10^(-2);
 
 tic;
-guess1=newton(x0,nMax,tol);
+newtong=newton(x0,nMax,tol);
 toc
 
 tic;
-guess2=steepest(x0,nMax,tol);
+steepestg=steepest(x0,nMax,tol);
 toc; 
 
 tic;
-guess3=broyden(x0,nMax,tol);
+broydeng=broyden(x0,nMax,tol);
+toc;
+
+tic;
+steep_newtong=steepest_newton(x0,nMax,tol1,tol);
 toc;
 
 function [o]=f(x)
@@ -35,7 +40,7 @@ function [j]=J(x)
     -2*x(1,:),-0.2*x(2,:)+0.01,1];
 end
 
-function guess=fixedpoint(x0,y0,nMax,tol)
+function [guess]=fixedpoint(x0,y0,nMax,tol)
     x=NaN(2,nMax+1);
     error=NaN(3,nMax+1);
     x(:,1)=[x0;y0];
@@ -120,6 +125,28 @@ function [guess]=broyden(x0,nMax,tol)
     xlabel("n")
     ylabel("log_{10}(error)")
     title("Broyden")
+end
+
+function [guess2]=steepest_newton(x0,nMax,tol1,tol2)
+    fun1=@(x) ([f(x);g(x);h(x)]);
+    fun2=@(x) norm([f(x);g(x);h(x)]);
+    Jfun=@(x) J(x);
+    verb=0;
+    Gfun=@(x) ([f(x);g(x);h(x)]);
+    type="wolfe";
+    [guess1,x1] = steepest_descent(fun2,Gfun,x0,tol1,nMax/2,type,verb);
+    [guess2,x2] = newton_method_nd(fun1,Jfun,guess1,tol2,nMax/2,verb);
+    x=cat(2,x1,x2);
+    figure()
+    error=NaN(4,length(x(1,:)));
+    error(1,:)=abs(g(x));
+    error(2,:)=abs(g(x));
+    error(3,:)=abs(h(x));
+    error(4,:)=sqrt(error(1,:).^2+error(2,:).^2+error(3,:).^2);
+    plot(1:length(x(1,:)),log10(error(4,:)))
+    xlabel("n")
+    ylabel("log_{10}(error)")    
+    title("Combined Newton/Steepest Descent")
 end
 
 function [r,rn] = broyden_method_nd(fun,B0,x0,tol,nmax,Bmat,verb)
@@ -244,7 +271,7 @@ end
 end
 
 function [guess]=steepest(x0,nMax,tol)
-    %fun=
+    fun=@(x) norm([f(x);g(x);h(x)]);
     Gfun=@(x) ([f(x);g(x);h(x)]);
     type="wolfe";
     verb=0;
@@ -381,4 +408,60 @@ if npn>tol
    fprintf('\n Newton method failed to converge, n=%d, res=%e\n',nmax,norm(Fn)); 
 end
 
+end
+
+function [alpha,x1] = line_search(fun,Gfun,x0,p,type,params)
+    %{
+    This Matlab function performs a back-tracking line search to find a value 
+    of alpha and x1 = x0 + alpha*p such that sufficient descent conditions have
+    been achieved for function fun in the direction p.  
+    
+    Inputs: 
+    fun - (function or function handle) function fun(x), x is
+    assumed to be an n x 1 array
+    Gfun - (function or function handle) Gradient vector of size n x 1
+    x0   - (n x 1 double) initial guess for minimum
+    p    - (n x 1 double) descent direction p 
+    type - (string) line search type. Options are 'wolfe', 'swolfe' (symmetric wolfe) and
+    'armijo' (default). 
+    params - (struct) parameters struct containing arguments for constants c1
+    and c2 (for descent and curvature conditions) and maxback (maximum number
+    of backtracking steps)
+    
+    Outputs:
+    r   - (n x 1 double) final estimate for the minimum
+    rn  - (n x niter double array) vector of iterates xn (used mainly for convergence /
+    testing purposes). 
+    
+    Instructor: Eduardo Corona
+    %}
+    
+    alpha=2; n=0; nmax = params.maxback; 
+    cond = false; 
+    f0 = fun(x0); 
+    Gdotp = Gfun(x0)'*p;
+    
+    % We run a while loop until the required descent conditions, encoded in the
+    % boolean variable cond are met, or n reaches nmax (maximum number of
+    % backtracking steps)
+    while n<=nmax & cond==false
+        alpha = alpha/2; 
+        x1 = x0+alpha*p;
+        if strcmp(type,'wolfe')
+            % Wolfe (Armijo sufficient descent and simple curvature conditions)
+            Armijo = fun(x1) <= f0 + params.c1*alpha*Gdotp; 
+            Curvature = Gfun(x1)'*p >= params.c2*Gdotp;
+            cond = Armijo & Curvature; 
+        elseif strcmp(type,'swolfe')
+            % Symmetric Wolfe (Armijo sufficient descent and symmetric curvature conditions)
+            Armijo = fun(x1) <= f0 + params.c1*alpha*Gdotp; 
+            Curvature = abs(Gfun(x1)'*p) <= params.c2*abs(Gdotp);
+            cond = Armijo & Curvature;
+        else
+            % Armijo (only sufficient descent condition)
+            Armijo = fun(x1) <= f0 + params.c1*alpha*Gdotp;
+            cond = Armijo; 
+        end
+        n=n+1; 
+    end
 end
