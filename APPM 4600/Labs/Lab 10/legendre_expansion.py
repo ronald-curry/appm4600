854a8a35c8493054bd6daeafc937559a313cbcf4
ronald-curry <rocu1744@colorado.edu>
2024-04-02 09:56:21 -0600

    addad cheby polys

--- APPM 4600/Labs/Lab 10/legendre_expansion.py
+++ APPM 4600/Labs/Lab 10/legendre_expansion.py
@@ -7,16 +7,16 @@ from scipy.integrate import quad as quad
 def driver():
 
 #  function you want to approximate
-    f = lambda x: math.exp(x)
-
+    #f = lambda x: math.exp(x)
+    f= lambda x: 1/(1+x**2)
 # Interval of interest
     a = -1
     b = 1
 # weight function
-    w = lambda x: 1.
-
+    w =  lambda x: 1
+    w2 = lambda x: 1/np.sqrt(1-x**2)
 # order of approximation
-    n = 2
+    n = 3
 
 #  Number of points you want to sample in [a,b]
     N = 1000
@@ -25,6 +25,24 @@ def driver():
 
     for kk in range(N+1):
       pval[kk] = eval_legendre_expansion(f,a,b,w,n,xeval[kk])
+
+    ''' create vector with exact values'''
+    fex = np.zeros(N+1)
+    for kk in range(N+1):
+        fex[kk] = f(xeval[kk])
+
+    plt.figure();
+    plt.plot(xeval,pval);
+    plt.show()
+
+    plt.figure();
+    err = abs(pval-fex)
+    plt.plot(xeval,np.log10(err)); 
+    plt.show()
+    
+    pval = np.zeros(N+1)
+    for kk in range(N+1):
+      pval[kk] = eval_cheby_expansion(f,a,b,w2,n,xeval[kk])
 
     ''' create vector with exact values'''
     fex = np.zeros(N+1)
@@ -66,6 +84,29 @@ def eval_legendre_expansion(f,a,b,w,n,x):
 
   return pval
 
+def eval_cheby_expansion(f,a,b,w,n,x):
+
+#   This subroutine evaluates the Chebyshev expansion
+
+#  Evaluate all the Legendre polynomials at x that are needed
+# by calling your code from prelab
+  p = eval_cheby(n, x)
+  # initialize the sum to 0
+  pval = 0.0
+  for j in range(0,n+1):
+      # make a function handle for evaluating phi_j(x)
+      phi_j = lambda x: eval_cheby(j, x)[j]
+      func1 = lambda x: phi_j(x)*f(x)*w(x)
+      func2 =lambda x: phi_j(x)**2*w(x)
+      # use the quad function from scipy to evaluate coeffs
+      funcj,err=quad(func1,a,b)
+      norm,err=quad(func2,a,b)
+      aj=funcj/norm
+      # accumulate into pval
+      pval = pval+aj*p[j]
+
+  return pval
+
 def eval_legendre(N,x):
     phi=np.zeros(N+1)
     phi[0]=1;
@@ -73,8 +114,18 @@ def eval_legendre(N,x):
         phi[1]=x;
     if N>1:
         for i in range(2,N+1):
-            phi[i]=1/(i)*((2*(i-1)+1)*x*phi[i-1]-i*phi[i-2])
+            phi[i]=1/(i)*((2*(i-1)+1)*x*phi[i-1]-(i-1)*phi[i-2])
     return phi
+
+def eval_cheby(n,x):
+    T=np.zeros(n+1)
+    T[0]=1;
+    if n>0:
+        T[1]=x;
+    if n>1:
+        for i in range(2,n+1):
+            T[i]=2*(i-1)*T[i-1]-T[i-2]
+    return T
 
 if __name__ == '__main__':
   # run the drivers only if this is called from the command line
