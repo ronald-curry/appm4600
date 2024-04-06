3f46ca1e2605fe50add01f5d51c2e5d7776324ec
ronald-curry <rocu1744@colorado.edu>
2024-04-06 10:11:38 -0600

    Update hw5q1.py

--- APPM 4600/Homework/hw5/hw5q1.py
+++ APPM 4600/Homework/hw5/hw5q1.py
@@ -5,9 +5,28 @@ import matplotlib.pyplot as plt
 def get_nodes(n,h):
     x=np.zeros(n+1)
     for i in range(n+1):
-        x[i]=-1+(i-1)*h+2/n
+        temp1=(2*i+1)*np.pi
+        temp2=2*(n+1)
+        x[i]=np.cos(temp1/temp2)
+    return x
+def get_nodes_e(n,h):
+    x=np.zeros(n+1)
+    for i in range(n+1):
+        x[i]=-1+(i-1)*h+h
     return x
    
+
+def phi(z,x):
+    p=1
+    for i in range(n+1):
+        p=p*(z-x[i])
+    return p
+def phi_e(z,x):
+    p=1
+    for i in range(n+1):
+        p=p*(z-x[i])
+    return p
+
 
 def weights(n,x):
     w=np.zeros(n+1)
@@ -24,8 +43,8 @@ def p(x,f,w,n,z):
     err=-5
     for j in range(n+1):
         if z!=x[j]:
-            top=top+w[j]/(z-x[j])*f(x[j])
-            bottom=bottom+w[j]/(z-x[j])
+            top=top+(w[j]/(z-x[j]))*f(x[j])
+            bottom=bottom+(w[j]/(z-x[j]))
         if z==x[j]:
             err=j
     pz=top/bottom
@@ -35,16 +54,29 @@ def p(x,f,w,n,z):
 
 
 f= lambda x: 1/(1+(16*x)**2)
-n=5
+n=80
 h=2/n
 x=get_nodes(n, h)
-plt.plot(x,f(x),'o')
+x_e=get_nodes_e(n, h)
+#plt.plot(x,f(x),'o')
 w=weights(n, x)
+w_e=weights(n,x_e)
 points=np.linspace(-1, 1,1001)
 data=f(points)
 poly=np.zeros(1001)
+poly_e=np.zeros(1001)
+ph=np.zeros(1001)
+ph_e=np.zeros(1001)
 for i in range(1001):
-    poly[i]=p(x,f,w,n,data[i])
-plt.plot(points,data)
-plt.plot(points,poly,'o')
-plt.show()
+    poly[i]=p(x,f,w,n,points[i])
+    poly_e[i]=p(x_e,f,w_e,n,points[i])
+    ph[i]=phi(points[i],x)
+    ph_e[i]=phi_e(points[i],x_e)
+#plt.plot(points,data)
+#plt.plot(points,poly)
+#plt.plot(points,np.log10(abs(data-poly)))
+#plt.plot(points,np.log10(abs(data-poly_e)))
+#plt.show()
+
+plt.plot(points,np.log10(ph))
+plt.plot(points,np.log10(ph_e))
