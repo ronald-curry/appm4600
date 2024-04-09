ece54d9aa2d25a62a8933e5721a48c0db3655caa
ronald-curry <rocu1744@colorado.edu>
2024-04-08 22:21:08 -0600

    prelab 11

---
+++ APPM 4600/Labs/Lab 11/prelab.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+f= lambda x: x**2
+a=0
+b=5
+n=5
+
+
+def trap(f,n,a,b):
+    x=np.linspace(a,b,n)
+    y=f(x)
+    total =y[0]+y[n-1]
+    for j in range (n-2):
+        total=total+2*y[j+1]
+    total=total/(n-1)
+    return total
+    
+trapz=trap(f,n,a,b)
+        
